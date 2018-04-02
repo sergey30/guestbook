1203c3f1d1ae59ffec1b914fb11750240ff91612
sergey30 <sergey30@users.noreply.github.com>
2018-04-02 16:36:09 +0700

    добавил кнопку удалить

--- js/ajax.js
+++ js/ajax.js
@@ -7,22 +7,23 @@ function sendAjaxForm(send_message, url) {
         success: function(response) {
             $("#send_message textarea").val(""); // очистка формы после отправки
             $("#result_form .message").remove();
+            $("#result_form .remove").remove();
             $("#result_form .message_data").remove();
         	result = $.parseJSON(response);
-            var id = "";
+            var remove = "remove";
             var first_name = "";
             var last_name = "";
             var date_created = "";
             var message = "";
             for (var i = 0; i < result.length; i++) {
-                id = result[i].id;
+                remove = result[i].id;
                 first_name = result[i].first_name;
                 last_name = result[i].last_name;
                 date_created = result[i].date_created;
                 message = result[i].message;
-                remove = "remove";
                 $("#result_form").prepend($("<div class='message'></div>").text(message));
-                $("#result_form").prepend($("<div class='message_data mt-3 pl-3 text-primary'></div>").text(first_name + " " + last_name + " " + date_created));
+                $("#result_form").prepend($("<a class='remove d-inline-block ml-5 text-danger'></a>").text(remove));
+                $("#result_form").prepend($("<span class='message_data d-inline-block mt-3 ml-3 text-primary'></span>").text(first_name + " " + last_name + " " + date_created));
     	    }
         }
     });
@@ -39,4 +40,13 @@ $(document).ready(function() {
             return false;
 		}
 	);
+});
+
+$(function() {
+   $("#result_form .remove").click(
+       function() {
+           event.preventDefault();
+           alert("6");
+       }
+   );
 });
